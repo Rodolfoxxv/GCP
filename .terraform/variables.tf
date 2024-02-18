490b7b49cc2c68eea3ee29ec14ce0db1ef99ef61
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-02-18 15:42:26 -0300

    Delete .terraform/variables.tf

--- .terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  type        = string
-  description = "The ID of the project where resources will be created."
-}
-
-variable "region" {
-  type        = string
-  description = "The region where resources will be created."
-}
