b4b1c582c16955c404997c737e8bff77451422b5
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-02-18 15:44:52 -0300

    gcp conf5
    bk dat

---
+++ terraform/gcs_bucket.tf
@@ -0,0 +1,13 @@
+resource "google_storage_bucket" "bucket_name" {
+  name     = "my-terraform-bucket-${random_integer.bucket_suffix.result}"
+  location = var.region
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "random_integer" "bucket_suffix" {
+  min = 100
+  max = 999
+}
