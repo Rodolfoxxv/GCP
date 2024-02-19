3379446be4a84fc9dc7cfe353d222aa74484a8cb
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-02-18 23:26:56 -0300

    finaliz
    asdasd

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
-output "bucket_name" {
-  value = google_storage_bucket.bucket_name.name
-}
+#output "bucket_name" {
+#  value = google_storage_bucket.bucket_name.name
+#}
 
 output "topic_name" {
   value = google_pubsub_topic.topic_name.name
