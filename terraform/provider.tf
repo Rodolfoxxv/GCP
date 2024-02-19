1f55a02e73231c2aa4053ca53da838acd888041d
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-02-18 21:31:35 -0300

    teste2
    d2

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,5 @@
 provider "google" {
-<<<<<<< HEAD
-  credentials = local.key_json
-=======
-  credentials = file("KEY.json")
->>>>>>> ac3c108d3075fe3049041996e35ab07533f78632
+  credentials = file("key.json")
   project     = var.project_id
   region      = var.region
 }
-
