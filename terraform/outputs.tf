09732adcc22f0db9525ff30db7ce8ccbf9dd65d7
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-02-27 22:21:05 -0300

    pi

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,17 +5,3 @@
 output "topic_names" {
   value = [for i in google_pubsub_topic.topic_name : i.name]
 }
-
-
-
-output "dataset_id" {
-  value = google_bigquery_dataset.dataset.dataset_id
-}
-
-output "dataset_location" {
-  value = google_bigquery_dataset.dataset.location
-}
-
-output "dataset_self_link" {
-  value = google_bigquery_dataset.dataset.self_link
-}
