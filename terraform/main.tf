1b6acd91a02f90e2cb6b39b390fbab978923b7b9
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-03-09 19:51:16 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,15 +5,18 @@ terraform {
       version = "3.5.0"
     }
   }
-}
 
-terraform {
   backend "gcs" {
     bucket = "entrada_terraform"
     prefix = "terraform/state"
   }
 }
 
+provider "google" {
+  credentials = file("creds.json")
+  project     = var.project_id
+  region      = var.region
+}
 
 resource "google_bigquery_dataset" "dataset" {
   count                       = lookup(var.env, "create_dataset", true) ? 1 : 0
