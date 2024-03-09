d4df67ba137797177094f2b3e83a798a019ba86a
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-03-09 19:06:13 -0300

    vvvv

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,3 @@
-terraform {
-  backend "gcs" {
-    bucket  = "entrada_terraform"
-    prefix  = "terraform/state"
-  }
-}
-
 resource "google_bigquery_dataset" "dataset" {
   count                       = lookup(var.env, "create_dataset", true) ? 1 : 0
   dataset_id                  = "projeto"
