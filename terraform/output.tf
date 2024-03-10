3b8775eb5ab790953c4cdbd113aa9b620ea43bec
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-03-10 12:19:34 -0300

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -11,25 +11,25 @@ output "schema_bucket_name" {
 }
 
 output "manual_dataset_id" {
-  value = [for i in google_bigquery_dataset.manual_dataset : i.dataset_id]
+  value = [for i in google_bigquery_dataset.dataset : i.dataset_id if i.dataset_id == var.manual_dataset]
 }
 
 output "manual_dataset_location" {
-  value = [for i in google_bigquery_dataset.manual_dataset : i.location]
+  value = [for i in google_bigquery_dataset.dataset : i.location if i.dataset_id == var.manual_dataset]
 }
 
 output "manual_dataset_link" {
-  value = [for i in google_bigquery_dataset.manual_dataset : i.self_link]
+  value = [for i in google_bigquery_dataset.dataset : i.self_link if i.dataset_id == var.manual_dataset]
 }
 
 output "auto_dataset_id" {
-  value = [for i in google_bigquery_dataset.auto_dataset : i.dataset_id]
+  value = [for i in google_bigquery_dataset.dataset : i.dataset_id if i.dataset_id == var.auto_dataset]
 }
 
 output "auto_dataset_location" {
-  value = [for i in google_bigquery_dataset.auto_dataset : i.location]
+  value = [for i in google_bigquery_dataset.dataset : i.location if i.dataset_id == var.auto_dataset]
 }
 
 output "auto_dataset_link" {
-  value = [for i in google_bigquery_dataset.auto_dataset : i.self_link]
+  value = [for i in google_bigquery_dataset.dataset : i.self_link if i.dataset_id == var.auto_dataset]
 }
