327a978ef983e3d83d688ecb874f81d18e4bec08
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-03-10 13:10:29 -0300

    Update table.tf

--- terraform/table.tf
+++ terraform/table.tf
@@ -1,12 +1,12 @@
 locals {
-  tables = { for table in var.tables : table["table_id"] => table }
+  tables = { for table in var.tables : table.table_id => table }
 }
 
 resource "google_bigquery_table" "tables" {
-  count = length(local.tables)
+  for_each = local.tables
 
-  dataset_id = local.tables[count.index]["dataset_id"]
-  table_id   = local.tables[count.index]["table_id"]
+  dataset_id = each.value.dataset_id
+  table_id   = each.value.table_id
 
   external_data_configuration {
     autodetect    = true
@@ -17,9 +17,9 @@ resource "google_bigquery_table" "tables" {
     }
 
     source_uris = [
-      "gs://${var.manual_updates_bucket}/Supermarket/${local.tables[count.index]["table_id"]}.csv",
+      "gs://${var.manual_updates_bucket}/Supermarket/${each.value.table_id}.csv",
     ]
   }
 
-  schema = data.google_storage_bucket_object.object[count.index].content
+  schema = data.google_storage_bucket_object.object[each.key].content
 }
