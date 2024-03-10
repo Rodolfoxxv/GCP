03cefa3e52f9b04c6cc6a0b23fed8da59712c231
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-03-10 18:23:50 -0300

    Update table.tf

--- terraform/table.tf
+++ terraform/table.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 resource "google_bigquery_table" "tables" {
-  for_each = values(local.tables)
+  for_each = local.tables
 
   dataset_id = each.value.dataset_id
   table_id   = each.value.table_id
