f583fbe6b007e2a4916119289e73293a20984f9f
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-03-11 00:18:06 -0300

    Update schemas.tf

--- terraform/schemas.tf
+++ terraform/schemas.tf
@@ -2,10 +2,10 @@ variable "schema_invoice" {
   default = <<EOF
 [
   {"name": "Order Id", "type": "STRING", "mode": "NULLABLE"},
-  {"name": "Date", "type": "DATE", "mode": "NULLABLE"},
+  {"name": "Date", "type": "STRING", "mode": "NULLABLE"}, 
   {"name": "Meal Id", "type": "STRING", "mode": "NULLABLE"},
   {"name": "Company Id", "type": "STRING", "mode": "NULLABLE"},
-  {"name": "Date of Meal", "type": "DATE", "mode": "NULLABLE"},
+  {"name": "Date of Meal", "type": "STRING", "mode": "NULLABLE"},  
   {"name": "Participants", "type": "STRING", "mode": "NULLABLE"},
   {"name": "Meal Price", "type": "FLOAT", "mode": "NULLABLE"},
   {"name": "Type of Meal", "type": "STRING", "mode": "NULLABLE"}
@@ -19,9 +19,9 @@ variable "schema_orderleads" {
   {"name": "Order Id", "type": "STRING", "mode": "NULLABLE"},
   {"name": "Company Id", "type": "STRING", "mode": "NULLABLE"},
   {"name": "Company Name", "type": "STRING", "mode": "NULLABLE"},
-  {"name": "Date", "type": "DATE", "mode": "NULLABLE"},
+  {"name": "Date", "type": "STRING", "mode": "NULLABLE"}, 
   {"name": "Order Value", "type": "FLOAT", "mode": "NULLABLE"},
-  {"name": "Converted", "type": "BOOLEAN", "mode": "NULLABLE"}
+  {"name": "Converted", "type": "INTEGER", "mode": "NULLABLE"} 
 ]
 EOF
 }
@@ -32,7 +32,8 @@ variable "schema_salesteam" {
   {"name": "Sales Rep", "type": "STRING", "mode": "NULLABLE"},
   {"name": "Sales Rep Id", "type": "STRING", "mode": "NULLABLE"},
   {"name": "Company Name", "type": "STRING", "mode": "NULLABLE"},
-  {"name": "Company Id", "type": "STRING", "mode": "NULLABLE"}
+  {"name": "Company Id", "type": "STRING", "mode": "NULLABLE"},
+  {"name": "string_field_0", "type": "STRING", "mode": "NULLABLE"}  
 ]
 EOF
 }
