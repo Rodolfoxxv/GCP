c9ad13fee6bfa28bc71f231a8930e0c3f11d7e8d
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-03-25 02:10:28 -0300

    Update load_data.sh

--- terraform/load_data.sh
+++ terraform/load_data.sh
@@ -7,18 +7,18 @@ PROJECT_ID="portfolioentrada"
 
 # Load data into tb_super_invoice
 bq --location=us-central1 load --source_format=CSV --skip_leading_rows=1 \
-  --replace --autodetect \
+  --truncate_table --autodetect \
   ${PROJECT_ID}:${DATASET}.tb_super_invoice \
   gs://${BUCKET}/supermarket/Invoices.csv
 
 # Load data into tb_super_orderleads
 bq --location=us-central1 load --source_format=CSV --skip_leading_rows=1 \
-  --replace --autodetect \
+  --truncate_table --autodetect \
   ${PROJECT_ID}:${DATASET}.tb_super_orderleads \
   gs://${BUCKET}/supermarket/OrderLeads.csv
 
 # Load data into tb_super_salesteam
 bq --location=us-central1 load --source_format=CSV --skip_leading_rows=1 \
-  --replace --autodetect \
+  --truncate_table --autodetect \
   ${PROJECT_ID}:${DATASET}.tb_super_salesteam \
   gs://${BUCKET}/supermarket/SalesTeam.csv
