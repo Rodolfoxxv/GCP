9048d8b0e526d66292ab005df857c168752b3d5e
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-03-25 02:11:35 -0300

    Update tables.tf

--- terraform/tables.tf
+++ terraform/tables.tf
@@ -8,7 +8,7 @@ resource "google_bigquery_table" "tb_super_invoice" {
   schema = var.schema_invoice
 }
 
-resource "google_bigquery_table" "tb_super_orderleads" {
+/*resource "google_bigquery_table" "tb_super_orderleads" {
   dataset_id = google_bigquery_dataset.manual.dataset_id
   table_id   = "tb_super_orderleads"
 
@@ -23,3 +23,4 @@ resource "google_bigquery_table" "tb_super_salesteam" {
   # Use the schema variable here
   schema = var.schema_salesteam
 }
+*/
