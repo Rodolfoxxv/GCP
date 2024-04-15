5e83a42cdd76350e256f3b40032412e782880b63
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-04-15 02:10:32 -0300

    Update etl_invoices.py

--- terraform/etl_invoices.py
+++ terraform/etl_invoices.py
@@ -1,21 +1,14 @@
-import csv
 import pandas as pd
 import duckdb
 
-# Lê o arquivo CSV com a biblioteca csv
-with open('C:\\Users\\rodol\\Downloads\\archive\\Invoices.csv', 'r') as f:
-    reader = csv.reader(f)
-    data = list(reader)
+# Lê o arquivo CSV com o pandas
+df = pd.read_csv('C:\\Users\\rodol\\Downloads\\archive\\Invoices.csv', quotechar='"')
 
 # Corrige a coluna Participants
-for row in data:
-    row[5] = row[5].replace("['", "").replace("']", "").replace("' '", ";")
-
-# Gera um DataFrame
-df = pd.DataFrame(data[1:], columns=data[0])
+df['Participants'] = df['Participants'].str.replace("[", "").str.replace("]", "").str.replace(", ", ";").str.replace('"', '')
 
 # Passa o DataFrame para o DuckDB
 duckdb_df = duckdb.from_df(df)
 
-
-
+# Salva o resultado como Invoices2.csv
+duckdb_df.to_csv('\\Invoices2.csv')
