312cedb8e308be5cea30cd02978a96e164dff695
RodolfoxvData <136007122+Rodolfoxxv@users.noreply.github.com>
2024-04-15 01:15:51 -0300

    Update load_data.sh

--- terraform/load_data.sh
+++ terraform/load_data.sh
@@ -1,24 +1,28 @@
-#!/bin/bash
-
 # Define the variables
 BUCKET="entrada_manual1"
 DATASET="manual"
 PROJECT_ID="affable-operand-420317"
 
-# Export the schemas to JSON files
-echo var.schema_invoice > schema_invoice.json
-echo var.schema_orderleads > schema_orderleads.json
-echo var.schema_salesteam > schema_salesteam.json
+# Define the schema for tb_super_salesteam
+schema_salesteam='[
+  {"name": "Sales_Rep",    "type": "STRING", "mode": "NULLABLE"},
+  {"name": "Sales_Rep_Id", "type": "STRING", "mode": "NULLABLE"},
+  {"name": "Company_Name", "type": "STRING", "mode": "NULLABLE"},
+  {"name": "Company_Id",   "type": "STRING", "mode": "NULLABLE"}
+]'
+
+# Export the schema to a JSON file
+echo ${schema_salesteam} > schema_salesteam.json
 
 # Load data into tb_super_invoice
 bq --location=us-central1 load --source_format=CSV --skip_leading_rows=1 \
-  --replace --schema=schema_invoice.json \
+  --replace --autodetect \
   ${PROJECT_ID}:${DATASET}.tb_super_invoice \
   gs://${BUCKET}/supermarket/Invoices.csv
 
 # Load data into tb_super_orderleads
 bq --location=us-central1 load --source_format=CSV --skip_leading_rows=1 \
-  --replace --schema=schema_orderleads.json \
+  --replace --autodetect \
   ${PROJECT_ID}:${DATASET}.tb_super_orderleads \
   gs://${BUCKET}/supermarket/OrderLeads.csv
 
